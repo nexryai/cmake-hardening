ba06908b108cfbcaaab50ac1763a7023507267e1
nexryai <nexryai@sda1.net>
2026-02-11 23:36:28 +0000

    WIP

--- Hardening.cmake
+++ Hardening.cmake
@@ -1,5 +1,6 @@
 include(CheckCXXCompilerFlag)
 include(FetchContent)
+include(ExternalProject)
 
 if(NOT CMAKE_CXX_COMPILER_ID MATCHES "Clang")
     message(FATAL_ERROR "Clang is required for these hardening features.")
@@ -13,22 +14,69 @@ endif()
 # You can use CMakeFetchContent to enforce these rules.
 #
 
-# Global Hardening Constants
+# Build musl from source with hardening
+set(MUSL_INSTALL_DIR "${CMAKE_BINARY_DIR}/musl_install")
 set(MUSL_TARGET "x86_64-linux-musl")
 
-set(HARDENING_COMMON_FLAGS "--target=${MUSL_TARGET} -flto=thin -fvisibility=hidden -fsanitize=cfi")
-set(HARDENING_C_FLAGS "${HARDENING_COMMON_FLAGS} -fstack-protector-strong -fstack-clash-protection -fvisibility=hidden -Wformat -Wformat-security -Werror=format-security -D_FORTIFY_SOURCE=3")
+ExternalProject_Add(musl_build
+    GIT_REPOSITORY "https://git.musl-libc.org/git/musl"
+    GIT_TAG "v1.2.5"
+    CONFIGURE_COMMAND <SOURCE_DIR>/configure
+        "CC=${CMAKE_C_COMPILER}"
+        "AR=llvm-ar"
+        "RANLIB=llvm-ranlib"
+        --prefix=${MUSL_INSTALL_DIR}
+        --target=${MUSL_TARGET}
+        --disable-shared
+    BUILD_COMMAND make CFLAGS=-fstack-protector-strong -j$(nproc)
+    INSTALL_COMMAND make install
+)
+
+# Global Hardening Constants
+set(HARDENING_COMMON_FLAGS
+    "--target=${MUSL_TARGET}"
+    "-flto=thin"
+    "-fvisibility=hidden"
+    "-fsanitize=cfi"
+    "-isystem ${MUSL_INSTALL_DIR}/include"
+)
+
+set(HARDENING_C_FLAGS
+    "${HARDENING_COMMON_FLAGS}"
+    "-fstack-protector-strong"
+    "-fstack-clash-protection"
+    "-Wformat"
+    "-Wformat-security"
+    "-Werror=format-security"
+    "-D_FORTIFY_SOURCE=3"
+)
 set(HARDENING_CXX_FLAGS "${HARDENING_C_FLAGS}")
 
-# Linker flags including static runtime enforcement
-set(HARDENING_LD_FLAGS "${HARDENING_COMMON_FLAGS} -fuse-ld=lld --rtlib=compiler-rt --unwindlib=libunwind -static -static-libgcc -static-pie -Wl,-z,relro -Wl,-z,now -Wl,-z,noexecstack")
+set(HARDENING_LD_FLAGS
+    "--target=${MUSL_TARGET}"
+    "-fuse-ld=lld"
+    "-stdlib=libc++"
+    "-nostdlib++"
+    "--rtlib=compiler-rt"
+    "--unwindlib=libunwind"
+    "-static"
+    "-static-pie"
+    "-L${MUSL_INSTALL_DIR}/lib"
+    "-lc"
+    "-lc++"
+    "-lc++abi"
+    "-lunwind"
+    "-Wl,--allow-multiple-definition" # Resolve conflict between musl and mimalloc
+    "-Wl,-z,relro"
+    "-Wl,-z,now"
+    "-Wl,-z,noexecstack"
+)
 
-# Apply globally to the current CMake project
-separate_arguments(HARDENING_C_FLAGS_LIST NATIVE_COMMAND "${HARDENING_C_FLAGS}")
-separate_arguments(HARDENING_LD_FLAGS_LIST NATIVE_COMMAND "${HARDENING_LD_FLAGS}")
+add_compile_options("${HARDENING_C_FLAGS}")
+add_link_options("${HARDENING_LD_FLAGS}")
 
-add_compile_options(${HARDENING_C_FLAGS_LIST})
-add_link_options(${HARDENING_LD_FLAGS_LIST})
+string(REPLACE ";" " " HARDENING_C_FLAGS_STR "${HARDENING_C_FLAGS}")
+string(REPLACE ";" " " HARDENING_LD_FLAGS_STR "${HARDENING_LD_FLAGS}")
 
 # Mimalloc Setup
 set(MI_SECURE "4" CACHE STRING "" FORCE)
@@ -45,7 +93,14 @@ FetchContent_Declare(
 FetchContent_MakeAvailable(mimalloc)
 
 function(setup_target_hardening TARGET)
-    target_link_libraries(${TARGET} PRIVATE mimalloc-static)
-
-    set_target_properties(${TARGET} PROPERTIES POSITION_INDEPENDENT_CODE ON)
+    add_dependencies(${TARGET} musl_build mimalloc-static)
+    
+    target_link_libraries(${TARGET} PRIVATE 
+        mimalloc-static
+    )
+    
+    set_target_properties(${TARGET} PROPERTIES 
+        POSITION_INDEPENDENT_CODE ON
+        INTERPROCEDURAL_OPTIMIZATION ON
+    )
 endfunction()
