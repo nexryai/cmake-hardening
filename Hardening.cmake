e958fc9d71cd667d4ffa8a83089b5862e8fdcc26
nexryai <nexryai@sda1.net>
2026-02-11 06:12:14 +0000

    Fix build

--- Hardening.cmake
+++ Hardening.cmake
@@ -15,8 +15,8 @@ endif()
 
 # Global Hardening Constants
 set(MUSL_TARGET "x86_64-linux-musl")
-set(HARDENING_COMMON_FLAGS "--target=${MUSL_TARGET} --rtlib=compiler-rt --unwindlib=libunwind -flto")
 
+set(HARDENING_COMMON_FLAGS "--target=${MUSL_TARGET} -fuse-ld=lld --rtlib=compiler-rt --unwindlib=libunwind -flto=thin -fvisibility=hidden -fsanitize=cfi")
 set(HARDENING_C_FLAGS "${HARDENING_COMMON_FLAGS} -fstack-protector-strong -fstack-clash-protection -fvisibility=hidden -Wformat -Wformat-security -Werror=format-security -D_FORTIFY_SOURCE=3")
 set(HARDENING_CXX_FLAGS "${HARDENING_C_FLAGS}")
 
@@ -24,14 +24,18 @@ set(HARDENING_CXX_FLAGS "${HARDENING_C_FLAGS}")
 set(HARDENING_LD_FLAGS "${HARDENING_COMMON_FLAGS} -static -static-libgcc -static-pie -Wl,-z,relro -Wl,-z,now -Wl,-z,noexecstack")
 
 # Apply globally to the current CMake project
-add_compile_options(${HARDENING_C_FLAGS})
-add_link_options(${HARDENING_LD_FLAGS})
+separate_arguments(HARDENING_C_FLAGS_LIST NATIVE_COMMAND "${HARDENING_C_FLAGS}")
+separate_arguments(HARDENING_LD_FLAGS_LIST NATIVE_COMMAND "${HARDENING_LD_FLAGS}")
+
+add_compile_options(${HARDENING_C_FLAGS_LIST})
+add_link_options(${HARDENING_LD_FLAGS_LIST})
 
 # Mimalloc Setup
 set(MI_SECURE "4" CACHE STRING "" FORCE)
 set(MI_BUILD_TESTS OFF CACHE BOOL "" FORCE)
 set(MI_BUILD_SHARED OFF CACHE BOOL "" FORCE)
-set(MI_BUILD_OBJECT ON CACHE BOOL "" FORCE)
+set(MI_BUILD_STATIC ON CACHE BOOL "" FORCE)
+set(MI_BUILD_OBJECT OFF CACHE BOOL "" FORCE)
 
 FetchContent_Declare(
     mimalloc
@@ -42,11 +46,7 @@ FetchContent_MakeAvailable(mimalloc)
 
 function(setup_target_hardening TARGET)
     # Mandatory linking
-    target_link_libraries(${TARGET} PRIVATE mimalloc)
-
-    # Mandatory CFI
-    target_compile_options(${TARGET} PRIVATE -fsanitize=cfi)
-    target_link_options(${TARGET} PRIVATE -fsanitize=cfi)
+    target_link_libraries(${TARGET} PRIVATE mimalloc-static)
 
     set_target_properties(${TARGET} PROPERTIES POSITION_INDEPENDENT_CODE ON)
 endfunction()
